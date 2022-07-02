fbd6769f63b1a118f86fa60fe2e9056facb6c4e8
Zanttu <zanttu@gmail.com>
2022-07-02 06:39:03 +0300

    Before implementing flash-logint and SQLAlchemy

--- app.py
+++ app.py
@@ -334,9 +334,51 @@ def add_nogo_date():
 
 
     
+#class User(flask_login.UserMixin):
+class User():
+    """
+    This provides default implementations for the methods that Flask-Login
+    expects user objects to have.
+    """
 
-class User(flask_login.UserMixin):
-    pass
+    # Python 3 implicitly set __hash__ to None if we override __eq__
+    # We set it back to its default implementation
+    __hash__ = object.__hash__
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_authenticated(self):
+        return self.is_active
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return str(self.id)
+        except AttributeError:
+            raise NotImplementedError("No `id` attribute - override `get_id`") from None
+
+    def __eq__(self, other):
+        """
+        Checks the equality of two `UserMixin` objects using `get_id`.
+        """
+        if isinstance(other, UserMixin):
+            return self.get_id() == other.get_id()
+        return NotImplemented
+
+    def __ne__(self, other):
+        """
+        Checks the inequality of two `UserMixin` objects using `get_id`.
+        """
+        equal = self.__eq__(other)
+        if equal is NotImplemented:
+            return NotImplemented
+        return not equal
     
 
 @login_manager.user_loader
