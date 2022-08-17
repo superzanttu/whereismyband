e512e1d82fc17780b25eaf956777f522805c6e77
Zanttu <zanttu@gmail.com>
2022-08-17 08:39:20 +0300

    Testing for pytest&vscode

---
+++ tests/test_pytest.py
@@ -0,0 +1,11 @@
+#from ..inc_dec import inc_dec    # The code to test
+import inc_dec 
+
+
+def test_increment():
+    assert inc_dec.increment(3) == 4
+
+def test_decrement():
+    assert inc_dec.decrements(3) == 4
+
+
