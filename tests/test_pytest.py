d0954eaac8a3a87bfa6e8536b94d9e46f628b034
Zanttu <zanttu@gmail.com>
2022-08-17 09:20:57 +0300

    Pytest finaly working in vscode

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,11 +1,20 @@
-#from ..inc_dec import inc_dec    # The code to test
-import inc_dec 
+import sys
+import os
+print ("Adding previous folder into path")
+current = os.path.dirname(os.path.realpath(__file__))
+print("  Current folder:",current)
+parent = os.path.dirname(current)
+print("  Parent folder:",parent)
+sys.path.append(parent)
 
+
+from inc_dec import *
+ 
+def test_kaa():
+    assert True == True
 
 def test_increment():
-    assert inc_dec.increment(3) == 4
+    assert increment(3) == 4
 
 def test_decrement():
-    assert inc_dec.decrements(3) == 4
-
-
+    assert decrements(3) == 4
